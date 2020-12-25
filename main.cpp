b0fb848bb4568b7c344278387e073c36f16b789b
Luna Sofia <nore0404@gmail.com>
2020-12-25 21:55:08 +0000

    Added alphanumeric checker for menuGUI and nested the menu

--- main.cpp
+++ main.cpp
@@ -9,6 +9,14 @@
 using namespace std;
 unsigned int microseconds;
 
+// Define numeric checker
+bool isNumeric(string str) {
+   for (int i = 0; i < str.length(); i++)
+      if (isdigit(str[i]) == false)
+         return false; //when a non numeric value is found, return false
+      return true;
+}
+
 // Standard variables
 double bal = 0; // Player balance
 int pre_id = 1; // Public previous player ID
@@ -20,10 +28,12 @@ int req_bait = 5; // Standard required bait
 int bait_usage = 5; // Standard bait usage
 int fish_amount = 0; // Defining fish amount
 int fish_caught; // Defining fish caught
+int fish_chance = 5; // Defining chance for fishing
 std::string fish_ans; // Defining fishing std::cin answer
 
 // MenuGUI function()
 void menuGUI() {
+    std::cout << "\n----------------------------\n";
     std::cout << "Type num of task to continue: \n";
     std::cout << "##########[ Menu ]##########\n";
     std::cout << "# [1] Go fishing           #\n";
@@ -37,76 +47,98 @@ void menuGUI() {
     std::cout << "############################\n\n";
 
     std::cout << "Num: ";
-    int menu_ans;
+    std::string menu_ans;
     std::cin >> menu_ans;
     std::cout << "\n";
 
-    if(menu_ans == 1) {
-        // Fishing-task
-        std::cout << "//Fishing//\n";
-        std::cout << "Fishing costs " << req_bait << " bait and " << tool_usage << "% of your fishing tool\n";
-        std::cout << "Would you like to continue?\n";
-        std::cout << "[Yes] or [No]\n\n";
-        std::cin >> fish_ans;
+    if(isNumeric(menu_ans)) {
+        if(menu_ans == "1") {
+            // Line break
+            std::cout << "----------------------------\n";
 
-        while(fish_ans == "Yes" && current_bait > req_bait && tool_wear > tool_usage) {
-            fish_caught = rand() % 5;
-            fish_amount = fish_amount + fish_caught;
-            tool_wear = tool_wear - tool_usage;
-            current_bait = current_bait - bait_usage;
-            std::cout << "\nBait = " << current_bait;
-            std::cout << "\nTool = " << tool_wear;
-            std::cout << "\nFish = " << fish_amount;
-            std::cout << "\n\n";
-            
-            std::cout << "//Fishing//\n";
+            // Fishing-task
+            std::cout << "\n//Fishing//\n";
             std::cout << "Fishing costs " << req_bait << " bait and " << tool_usage << "% of your fishing tool\n";
             std::cout << "Would you like to continue?\n";
             std::cout << "[Yes] or [No]\n\n";
+            std::cin >> fish_ans;
+
+            while(fish_ans == "Yes" && current_bait > req_bait && tool_wear > tool_usage) {
+                fish_caught = rand() % fish_chance;
+                fish_amount = fish_amount + fish_caught;
+                tool_wear = tool_wear - tool_usage;
+                current_bait = current_bait - bait_usage;
+
+                // Line break
+                std::cout << "----------------------------\n";
+
+                std::cout << "\nBait = " << current_bait;
+                std::cout << "\nTool = " << tool_wear;
+                std::cout << "\nFish = " << fish_amount;
+                std::cout << "\n\n";
+            
+                // Line break
+                std::cout << "----------------------------\n";
+
+                std::cout << "\n//Fishing//\n";
+                std::cout << "Fishing costs " << req_bait << " bait and " << tool_usage << "% of your fishing tool\n";
+                std::cout << "Would you like to continue?\n";
+                std::cout << "[Yes] or [No]\n\n";
 
             std::cin >> fish_ans;
+            }
+            if(fish_ans == "No") {
+                std::cout << "\n----------------------------\n";
+                std::cout << "\nSending you back to the menu\n\n";
+                menuGUI();
+            } 
+            else if(current_bait >! req_bait) {
+                std::cout << "\n----------------------------\n";
+                std::cout << "\n### You do not have enough bait ###\n\n";
+                menuGUI();
+            }
+            else if(tool_wear >! tool_usage) {
+                std::cout << "\n----------------------------\n";
+                std::cout << "\n### Your tool does not have enough wear ###\n\n";
+                menuGUI();
+            }
         }
-        if(fish_ans == "No") {
-            std::cout << "Sending you back to the menu\n\n";
-            menuGUI();
-        } 
-        else if(current_bait >! req_bait) {
-            std::cout << "\n### You do not have enough bait ###\n\n";
+        else if(menu_ans == "2") {
+            // Line break
+            std::cout << "----------------------------\n";
+
+            // Show inventory
+            std::cout << "Bait = " << current_bait << "     ";
+            std::cout << "Tool = " << tool_wear;
+            std::cout << "\nFish = " << fish_amount << "      ";
+            std::cout << "Chances = 1/" << fish_chance;
+            std::cout << "\n\n";
             menuGUI();
         }
-        else if(tool_wear >! tool_usage) {
-            std::cout << "\n### Your tool does not have enough wear ###\n\n";
+        else if(menu_ans == "3") {
+            // Se balance
+            std::cout << "//Balance//";
+        }
+        else if(menu_ans == "4") {
+            // Go to market
+            std::cout << "//Market//";
+        }
+        else if(menu_ans == "5") {
+            // Go to black market
+            std::cout << "//B-Market//";
+        }
+        else if(menu_ans == "6") {
+            // Go to dealership
+            std::cout << "//Dealership//";
+        }
+        else {
+            // Send back to menu if answer is not valid
+            std::cout << "Answer not accepted";
             menuGUI();
         }
-    }
-    else if(menu_ans == 2) {
-        // Show inventory
-        std::cout << "Bait = " << current_bait << "     ";
-        std::cout << "Tool = " << tool_wear;
-        std::cout << "\nFish = " << fish_amount << "      ";
-        std::cout << "Cash = " << bal;
-        std::cout << "\n\n";
-        menuGUI();
-    }
-    else if(menu_ans == 3) {
-        // Se balance
-        std::cout << "//Balance//";
-    }
-    else if(menu_ans == 4) {
-        // Go to market
-        std::cout << "//Market//";
-    }
-    else if(menu_ans == 5) {
-        // Go to black market
-        std::cout << "//B-Market//";
-    }
-    else if(menu_ans == 6) {
-        // Go to dealership
-        std::cout << "//Dealership//";
     }
     else {
-        // Send back to menu if answer is not valid
-        std::cout << "Answer not accepted";
+        std::cout << "Answer is not alphanumeric";
         menuGUI();
     }
 }
@@ -121,9 +153,8 @@ int main() {
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
     std::cout << std::endl;
-    std::cout << "-----------------------------\n";
 
-    menuGUI ();
+    menuGUI();
 
 }
 
